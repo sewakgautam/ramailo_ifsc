a46fe0de359b1d3ffaa047dbaa464fb7e78f466b
sewakgautam <sewak.gautam58@gmail.com>
2023-01-10 15:57:22 +0545

    🐞 fix: Changes on config
    Changes on the config file with the cloud config

--- config.ts
+++ config.ts
@@ -2,20 +2,19 @@ import 'dotenv/config';
 
 export const NODE_ENV = process.env.NODE_ENV;
 
-export const TOKENS = {
-  JWT_SECRET: process.env.JWT_SECRET,
-};
-export const DB = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-};
+// export const DB = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// };
 
-export const SMTP = {
-  host: process.env.SMTP_HOST,
-  user: process.env.SMTP_USER,
-  pass: process.env.SMTP_PASS,
-  port: process.env.SMTP_PORT,
+// for hosting
+export const DB = {
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'ifsc',
 };
