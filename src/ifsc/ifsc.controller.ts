e8439fd26b6bde36cfab4533ad130fb8536d44da
sewakgautam <sewak.gautam58@gmail.com>
2023-01-10 14:51:43 +0545

    📃 docs: Added Description
    Added Description of swagger api

--- src/ifsc/ifsc.controller.ts
+++ src/ifsc/ifsc.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { IfscService } from './ifsc.service';
 import { findIfscDto } from './dto/create-ifsc.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('ifsc')
 @ApiTags('IFSC')
@@ -9,21 +9,32 @@ export class IfscController {
   constructor(private readonly ifscService: IfscService) {}
 
   @Get('bank')
+  @ApiOperation({ summary: 'Find All Bank' })
   findBank() {
     return this.ifscService.findBank();
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Find State of Individual bank by Providing Bank Name',
+  })
   getBankState(@Query('bank') bank: string) {
     return this.ifscService.getBankState(bank);
   }
 
   @Get(':bank')
+  @ApiOperation({
+    summary: 'Find city of Individual bank by providing Bank Name and state',
+  })
   findCity(@Param('bank') bank: string, @Query('state') state: string) {
     return this.ifscService.findCity(bank, state);
   }
 
   @Get(':bank/:state')
+  @ApiOperation({
+    summary:
+      'Find Branch of Individual bank by providing Bank Name, state and city',
+  })
   findBranch(
     @Param('bank') bank: string,
     @Param('state') state: string,
@@ -33,6 +44,10 @@ export class IfscController {
   }
 
   @Post()
+  @ApiOperation({
+    summary:
+      'Find full details of Individual bank of individual branch by providing Bank Name, state , city and branch Name ',
+  })
   findIfsc(@Body() findIfscDto: findIfscDto) {
     return this.ifscService.findIfsc(findIfscDto);
   }
